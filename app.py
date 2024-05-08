c6ba68e0f716540133b9310895c9f8ec2b28371f
Finay <55969746+Finay@users.noreply.github.com>
2024-05-08 15:09:22 +0900

    Test docs directory

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ FLATPAGES_ROOT = 'templates/pages'
 FLATPAGES_AUTO_RELOAD = DEBUG
 FLATPAGES_EXTENSION = ['.md', '.html']
 FREEZER_BASE_URL = "https://finay.github.io/"
+FREEZER_DESTINATION = "docs"
 
 app = Flask(__name__)
 app.config.from_object(__name__)
