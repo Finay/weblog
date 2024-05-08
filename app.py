b2f79a18bb3193ef4dc6882af508ceea2759a32a
Finay <55969746+Finay@users.noreply.github.com>
2024-05-08 10:48:37 +0900

    Add helper functions and update general site layout

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 import jinja2.exceptions
 from flask import Flask, render_template, abort
+import json
+from os import listdir
+from flaskext.markdown import Markdown
 
 app = Flask(__name__)
+Markdown(app)
+
+
+def getPostInfo(file):
+    with open(f'templates/pages/posts/{file}') as fd:
+        lines = [i.strip() for i in fd.readlines()]
+        if '{#' in lines and '#}' in lines:
+            lines = lines[lines.index('{#')+1:lines.index('#}')]
+    return json.loads("".join(lines))
+
+
+def getPostFiles():
+    return listdir('templates/pages/posts')
+
+
+def getPostsInfo():
+    post_files = getPostFiles()
+    posts_info = [getPostInfo(file) for file in post_files]
+    return [*zip(post_files, posts_info)]
 
 
 @app.route('/blog/')
@@ -9,11 +31,30 @@ def blogHome():
     return render_template('pages/index.html')
 
 
+@app.route('/blog/about')
+def blogAbout():
+    return render_template('pages/about.html')
+
+
+@app.route('/blog/tags')
+def blogTags():
+    return render_template('pages/tags.html')
+
+
 @app.route('/blog/<path>')
 def blogPost(path):
     path = path.replace(".", "")
     try:
-        return render_template(f'pages/{path}.html')
+        return render_template(f'pages/posts/{path}.html')
+    except jinja2.exceptions.TemplateNotFound:
+        abort(404)
+
+
+@app.route('/wip/<path>')
+def workInProgress(path):
+    path = path.replace(".", "")
+    try:
+        return render_template(f'wip/{path}.html')
     except jinja2.exceptions.TemplateNotFound:
         abort(404)
 
